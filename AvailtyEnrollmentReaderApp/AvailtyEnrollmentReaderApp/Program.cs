af19653bb2c188306e92bbe83ff3ced7fafbf15e
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-03 08:04:37 -0600

    Created AvailtyEnrollmentReaderApp solution.

---
+++ AvailtyEnrollmentReaderApp/AvailtyEnrollmentReaderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AvailtyEnrollmentReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
